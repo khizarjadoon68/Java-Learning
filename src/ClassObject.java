d59d95cc9a20a01a14c6b249c052bf30090391c1
Khizar jadoon <khizarjadoon68@gmail.com>
2026-06-03 22:53:37 +0500

    StaticMember file added

---
+++ src/ClassObject.java
@@ -0,0 +1,29 @@
+class Box {
+    // Properties / Instance Member variable
+    private int length, breadth, height;
+
+    //Methods // Instance member functions
+    public void setDimension(int l, int b, int h) {
+        length = l;
+        breadth = b;
+        height = h;
+    }
+
+    public void showDimension() {
+        System.out.println("l=" + length);
+        System.out.println("b=" + breadth);
+        System.out.println("h=" + height);
+
+    }
+
+}
+class Example {
+    public static void main() {
+        Box smallBox = new Box(); // Reference variable / Object
+        smallBox.setDimension(13, 23, 4);
+        smallBox.showDimension();// garbage
+        smallBox = new Box(); // New Object
+        smallBox.showDimension(); // Show default (0,0,0)
+    }
+}
+
---
+++ src/StaticMember.java
@@ -0,0 +1,33 @@
+
+// A variable that shared between all objects of a class ( instead of each object have its copy ).
+public class StaticMember{
+
+    int x; //Instance variable .
+    static int y ; // static variable .
+
+    public void fun1 (){} //instance member function .
+
+    //static member only access static variable not instance variable .
+    public static void fun2 (){}// static member function .
+
+    //static variable never used in method .
+    //But we can have static class .
+    static class Test{
+        public static String country ="Pakistan";
+    } //static class .
+
+    public static void main(String[] args){
+
+        Example ex1 = new Example();
+        Example ex2 = new Example();
+
+        // Static variable and function are call with Class name not with objects
+        /*Same type of function can call each other . In this example we can call y and fun2 without using the class
+         name because they are same type of variable and function .s
+         */
+
+        y = 5 ;
+        fun2();
+        System.out.println(Test.country);
+    }
+}
